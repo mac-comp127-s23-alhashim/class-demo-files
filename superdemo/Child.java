7fe5dbda26a4dbcff31fcef58d9042edf65dac1d
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-03-01 15:10:51 -0600

    3/1 class sec 4

--- superdemo/Child.java
+++ superdemo/Child.java
@@ -5,6 +5,9 @@ public class Child extends Parent {
 
     public Child() {
         super("parent from child");
+        // super();
         ch = "child";
+        // p = "parent from child";
+        // sp = "super parent from child";
     }
 }
--- superdemo/Parent.java
+++ superdemo/Parent.java
@@ -3,10 +3,10 @@ package superdemo;
 public class Parent extends SuperParent {
     String p;
 
-    // public Parent() {
-    //     // super();
-    //     this.p = "parent";
-    // }
+    public Parent() {
+        super();
+        this.p = "parent";
+    }
 
     public Parent(String p) {
         super("super parent from parent");
