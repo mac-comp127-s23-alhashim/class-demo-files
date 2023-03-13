1a0c9cf4cd6e90d9b6cf1868d00e971c8bb0151f
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-03-12 23:28:11 -0500

    seperate examples

--- lambdaexp/LambdaTest_Sort.java
+++ lambdaexp/LambdaTest_Sort.java
@@ -8,8 +8,11 @@ import java.awt.Toolkit;
 
 /*
  * Source: Horstmann2021 Core Java Volume I: Fundamentals, Chapter 6, p. 6.3
+ * 
+ * Illustrator of lambda expression that take the role (provide implementation) of the compare 
+ * method of the Comaraator functional interface
  */
-public class LambdaTest {
+public class LambdaTest_Sort {
     public static void main(String[] args) {
         String[] planets = new String[] {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Naptune"};
         System.out.println(Arrays.toString(planets));
@@ -23,16 +26,5 @@ public class LambdaTest {
         System.out.println("Sorted by length");
         Arrays.sort(planets, (first, second) -> first.length() - second.length());
         System.out.println(Arrays.toString(planets));
-
-
-        Timer t = new Timer(1000, event -> {
-            System.out.println("The time is " + new Date());
-            Toolkit.getDefaultToolkit().beep();
-        });
-        t.start();
-
-        // keep program running untill user select "OK"
-        JOptionPane.showMessageDialog(null, "Quit program?");
-        System.exit(0);
     }
 }
