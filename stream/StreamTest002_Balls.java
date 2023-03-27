b5e3a998c3667b93cc2190f14b59ba22f4be13d7
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-03-27 12:51:26 -0500

    update ball stream example

--- stream/StreamTest002_Balls.java
+++ stream/StreamTest002_Balls.java
@@ -6,6 +6,9 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 enum Color {
     RED,
     BLUE,
@@ -69,15 +72,24 @@ class Prep2 {
             System.out.println(ball);
         }
     }
+
+    public static void printBallsIds(List<String> list, String msg) {
+        System.out.println();
+        System.out.println("===== " + msg + " (" + list.size() + ") ===================================");
+        for(String ball : list) {
+            System.out.println(ball);
+        }
+    }
 }
 
 
 // we want to get the largest 10 red balls
 class Test {
     public static void main(String[] args) {
-        List<Ball> balls = Prep2.generateRandomeBalls(100, 14);
-        Prep2.printBalls(balls, "All Balls");
+        List<Ball> balls = Prep2.generateRandomeBalls(1_000_000_0, 14);
 
+        // Non-stream approach =====================================
+        long startTime = System.currentTimeMillis();
         // filter red balls
         List<Ball> redBalls = new ArrayList<>();
         for(Ball ball : balls) {
@@ -85,17 +97,40 @@ class Test {
                 redBalls.add(ball);
             }
         }
-        Prep2.printBalls(redBalls, "Red Balls");
 
-        // sort red balls
+        // sort
         Collections.sort(redBalls, new Comparator<Ball>() {
             public int compare(Ball ball1,  Ball ball2) {
-                return -Double.compare(ball1.getSize(), ball2.getSize());
+                return Double.compare(ball1.getSize(), ball2.getSize());
             }
         });
-        Prep2.printBalls(redBalls, "Red Balls Sorted");
-
         
+        // get top 10
+        List<Ball> redBallsTop10 = new ArrayList<>();
+        for(int i=0; i<10; i++) {
+            redBallsTop10.add(redBalls.get(i));
+        }
 
+        // get balls' ids
+        List<String> redBallTop10Ids = new ArrayList<>();
+        for(Ball ball : redBallsTop10) {
+            redBallTop10Ids.add(ball.getId());
+        }
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("Non-Stream: " + estimatedTime);
+
+
+        // Stream Approach =========================================
+        startTime = System.currentTimeMillis();
+        List<String> redBallIds_stream = 
+            // balls.parallelStream()
+            balls.stream()
+            .filter(b -> b.getColor() == Color.RED)
+            .sorted(comparing(Ball::getSize))
+            .limit(10)
+            .map(Ball::getId)
+            .collect(toList());
+        estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("Stream: " + estimatedTime);
     }
 }
