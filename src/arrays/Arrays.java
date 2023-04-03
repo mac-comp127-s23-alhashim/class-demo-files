9c3919ddf80c8097917dff5076377bf7f7a4dc23
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2023-04-03 00:58:17 -0500

    array prep

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        a[0]=11; a[1]=22; a[2]=33; a[3]=44; a[4]=55;
+
+        String[] s = new String[3];
+        s[0] = "AA";
+        s[1] = "BB";
+        s[2] = "CC";
+
+        String[] s2 = {new String(), new String(), new String()};
+        
+    }
+}
